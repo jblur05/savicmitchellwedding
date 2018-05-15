f70794baf9482c7b516ef2a772b8e2744746f945
James <jblur05>
2018-05-14 22:28:56 -0600

    Initial Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+<body>
+    <nav class="navbar has-shadow">
+        <div class="navbar-brand">
+            <p class="navbar-item title tangerine-font-bold">Savic-Mitchell Wedding</p>
+            <p class="navbar-item subtitle tangerine-font-regular">August 18 2018</p>
+        </div>
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="field is-grouped">
+                    <!-- <p class="control">
+                        <a class="button">About</a>
+                    </p>
+                    <p class="control">
+                        <a class="button">RSVP</a>
+                    </p>
+                    <p class="control">
+                        <a class="button">Pictures</a>
+                    </p> -->
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="frame">
+        <Carousel/>
+    </div>
+</body>
+</template>
+
+<script>
+import Carousel from '@/components/carousel/Carousel.vue'
+export default {
+  name: 'Home',
+  components: {
+    Carousel
+  }
+}
+</script>
+
+<style type="text/css">
+@import '../css/main.css'
+</style>
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="carousel-view">
+    <transition class='carousel' name="carouseltrans" mode="out-in">
+        <img class="eng-img" :src="visibleImageURL" :key="visibleImage"/>
+    </transition>
+    <!-- <div class='carousel-controls'>
+      <button class='carousel-controls__button' @click="previous">prev</button>
+      <button class='carousel-controls__button' @click="next">next</button>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      numPics: 9,
+      visibleImage: 0,
+      slideFilePrefix: 'eng',
+      slideFilePostfix: 'jpg',
+      interval: undefined
+    }
+  },
+  computed: {
+    visibleImageURL: function () {
+      return '/static/' + this.slideFilePrefix + this.visibleImage + '.' + this.slideFilePostfix
+    }
+  },
+  methods: {
+    next () {
+      this.visibleImage = (this.visibleImage + 1) === this.numPics ? 0 : this.visibleImage + 1
+    },
+    previous () {
+      this.visibleImage = (this.visibleImage - 1) < 0 ? this.numPics - 1 : this.visibleImage - 1
+    }
+  },
+  created: function () {
+    const self = this
+    this.interval = setInterval(function () {
+      self.next()
+    }, 8000)
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+    clearInterval(this.interval)
+  }
+}
+</script>
+
+<style>
+.carouseltrans-enter-active, .carouseltrans-leave-active {
+  transition: all 2s;
+}
+
+.carouseltrans-enter, .carouseltrans-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.carousel-controls {
+  position: absolute;
+}
+
+.carousel-view {
+  overflow: hidden;
+  max-width: var(--carousel-img-max-width);
+  max-height: var(--carousel-img-max-height);
+}
+
+.frame {
+    margin: auto;
+    background-color: white;
+    min-width: var(--carousel-frame-min-width);
+    min-height: var(--carousel-frame-min-height);
+    padding: var(--carousel-frame-padding);
+}
+
+@media screen and (min-width: 800px) {
+
+}
+</style>
